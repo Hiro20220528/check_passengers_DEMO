a5c9beab9ad2e24641f8f4e8e2466137108cb8ed
Okami Hiro <yy20soccer@gmail.com>
2023-06-15 22:20:15 +0900

    add contracts files

---
+++ blockchain/contracts/credit.sol
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: MIT
+import "./verify.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.6.11",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
