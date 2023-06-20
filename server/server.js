993ffb558c7eda6d27be185c54b60cf547341312
Okami Hiro <yy20soccer@gmail.com>
2023-06-20 13:11:12 +0900

    rootサイトの作成

--- server/server.js
+++ server/server.js
@@ -14,12 +14,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public'))); // publicフォルダの中身を静的ファイル
 
-
+// ページ一覧を表示 get
 app.get('/', (req, res) => {
           res.sendFile(__dirname + '/public/index.html');
 });
 
+// 乗車人数予約画面 get
+app.get('/reserve', (req, res) => {
+          res.sendFile(__dirname + '/public/reserve.html');
+});
 
+// 乗車人数確定画面へpost
+
+
+
+
+// 乗車人数確定画面 get
+
+
+// サーバーを起動
 server.listen(PORT, () => {
           console.log('lisning on *:3000');
 });
